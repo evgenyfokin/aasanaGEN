f369d010569142de0d02dc4b70a2286330ee1960
evgenyfokin <fokin_e@inbox.ru>
2022-06-24 00:58:18 +0700

    Added header

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 'use strict';
 
-window.onload = func;
+// window.onload = generateAsans;
 
-function func() {
-    // document.getElementById("jump_to_this_location").scrollIntoView(alignToTop);
+function generateAsans() {
     document
-        // .getElementById('jump_to_this_location')
-        // .scrollIntoView({ behavior: 'smooth' });
-        document.getElementById('vh').scrollIntoView({block: "center", behavior: "smooth"});
+        .getElementById('tr')
+        .scrollIntoView({ block: 'start', behavior: 'smooth' });
+    let load = document.getElementById('loader');
+    // load.className = 'loader';
+    load.setAttribute('style', 'opacity: 1');
+    // setTimeout((load.innerHTML = 'Hello world'), 5);
+    // return setTimeout(document.getElementById('loader').remove('loader'), 3)
+    // document.getElementById('loader').remove('loader');
+    setTimeout(clear, 1000);
+}
+
+function clear() {
+    // let clear = document.getElementById('loader').removeClass('loader');
+    // // .classList.remove('loader');
+    let load = document.getElementById('loader');
+    // load.classList.remove('loader');
+    load.setAttribute('style', 'display: none');
 }
