dcc08fd64db6b68b1034c6917bf5cd5407728e66
evgenyfokin <fokin_e@inbox.ru>
2022-06-24 18:11:29 +0700

    added up button

--- script.js
+++ script.js
@@ -3,16 +3,14 @@
 // window.onload = generateAsans;
 
 function generateAsans() {
-    document
-        .getElementById('tr')
-        .scrollIntoView({ block: 'start', behavior: 'smooth' });
     let load = document.getElementById('loader');
     // load.className = 'loader';
     load.setAttribute('style', 'opacity: 1');
     // setTimeout((load.innerHTML = 'Hello world'), 5);
     // return setTimeout(document.getElementById('loader').remove('loader'), 3)
     // document.getElementById('loader').remove('loader');
-    setTimeout(clear, 1000);
+    setTimeout(clear, 2000);
+    setTimeout(getDown, 1900);
 }
 
 function clear() {
@@ -21,4 +19,16 @@ function clear() {
     let load = document.getElementById('loader');
     // load.classList.remove('loader');
     load.setAttribute('style', 'display: none');
+}
+
+function getDown() {
+    document
+        .getElementById('scroll-down')
+        .scrollIntoView({ block: 'start', behavior: 'smooth' });
+}
+
+function getUp() {
+    document
+        .getElementById('scroll-up')
+        .scrollIntoView({ blocl: 'start', behavior: 'smooth' });
 }
